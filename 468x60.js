0517a5cc1c48e5e803969b6d7b256569c15a5682
PancenOye007 <115642214+PancenOye007@users.noreply.github.com>
2024-12-16 17:21:11 +0700

    Create 468x60.js

---
+++ 468x60.js
@@ -0,0 +1,8 @@
+	atOptions = {
+		'key' : 'c6cf35a1c4a4d4d41497f4936920b68d',
+		'format' : 'iframe',
+		'height' : 60,
+		'width' : 468,
+		'params' : {}
+	};
+src="//www.highperformanceformat.com/c6cf35a1c4a4d4d41497f4936920b68d/invoke.js">
